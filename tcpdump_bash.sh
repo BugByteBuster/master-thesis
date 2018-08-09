a55206066d552a4430ab27774682f4187c02a487
vidyadharreddy18 <vidyadharlippy10@gmail.com>
2018-08-09 11:40:12 +0200

    Create tcpdump_bash.sh

---
+++ tcpdump_bash.sh
@@ -0,0 +1,36 @@
+#sudo timeout 100 tcpdump -i enp0s3 greater 1500 | awk '{print $1, $15}' > packet_info.txt
+
+declare -a time_interval
+declare -a packet_count
+
+
+#time_array=($(cat packet_info.txt | awk '{ print $1 }'))
+#packetsize_array=($(cat packet_info.txt | awk '{ print $2 }'))
+
+#echo all : ${time_array[*]}
+#echo one : ${time_array[1]}
+#echo all : ${packetsize_array[*]}
+#echo one : ${packetsize_array[1]}
+#echo length of array
+#echo ${#time_array[@]}
+
+
+#packet_count=($(uniq -c --check-chars=2 /home/vidyadhar/packet_info.txt | awk '{print $1}'))
+#time_interval=($(uniq -c --check-chars=2 /home/vidyadhar/packet_info.txt | awk '{print $2}'))
+uniq -c --check-chars=2 /home/vidyadhar/packet_info.txt | sed -e 's/^\(.\{10\}\).*$/\1/'> count_time.txt
+
+echo ${packet_count[*]}
+echo ${time_interval[*]}
+
+
+gnuplot -persist <<-EOFMarker
+	set title "packet size vs time"
+	set xlabel "time"
+	set ylabel "size of packet"
+	set style data histogram
+	set style histogram cluster gap 1
+        set style fill solid border -1
+	set grid
+	plot "/home/vidyadhar/count_time.txt" using 1:xticlabels(2) with histogram
+EOFMarker
+
