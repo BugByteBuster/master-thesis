23a59b0eeb4433e55e9bf34331458bdbb30ae805
vidyadharreddy18 <vidyadharlippy10@gmail.com>
2018-08-13 10:41:55 +0200

    Create get_details.py

---
+++ test1/get_details.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+import paramiko
+import time
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(hostname='134.138.212.12', username='ezpedvi', password='9505100403@usha')
+chan = ssh.invoke_shell()
+
+time.sleep(3)
+#ssh from ez to sc1
+command = chan.send("ssh root@10.80.68.37\n")
+time.sleep(5)
+opt = chan.recv(9999)
+print "opt{}".format(opt)
+chan.send("rootroot\n")
+time.sleep(3)
+opt2 = chan.recv(9999)
+print opt2
+
+#ssh from  sc1 to pl3
+chan.send("ssh PL-3\n")
+time.sleep(3)
+opt3 = chan.recv(9999)
+print opt3
+
+#file transfer Pl-3 to sc-1
+chan.send("./send_files.sh\n")
+time.sleep(10)
+chan.send("exit\n")
+time.sleep(3)
+opt4= chan.recv(9999)
+print opt4
+
+#file transfer from sc1- ez
+chan.send("scp -r /root/packets ezpedvi@134.138.212.12:/home/ezpedvi/packets\n")
+time.sleep(2)
+chan.send("9505100403@usha\n")
+time.sleep(10)
+opt5=chan.recv(9999)
+print opt5
+chan.send("rm -rf packets\n")
+time.sleep(4)
+chan.send("mkdir packets\n")
+time.sleep(4)
+chan.send("exit\n")
+
+opt6=chan.recv(9999)
+print opt6
+ssh.close()
+
+'''
+script to run commands over ssh channel and get the files, VM to ez to SC1 to PL3 sends back files exit sl3 sends back files to ez
+'''
