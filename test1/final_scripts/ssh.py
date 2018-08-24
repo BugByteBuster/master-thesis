b70f524f6ff7ca22971f012ea0773e27214531ed
vidyadharreddy18 <vidyadharlippy10@gmail.com>
2018-08-24 10:29:38 +0200

    Create ssh.py

---
+++ test1/final_scripts/ssh.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import paramiko
+import time
+import re
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+t=ssh.connect(hostname='10.80.100.102', username='root', password='rootroot')
+print t
+chan = ssh.invoke_shell()
+
+for i in range(0, 5000):
+	print "fetching files for the {}st time".format(i)
+	chan.send("/cluster/./send_files.sh\n")
+        while True:
+        	resp = chan.recv(9999)
+		print resp
+		if "password:" in resp:
+                	chan.send("9505100403@usha\n")
+		if "SC-1:" in resp:
+			break
+        chan.send("exit\n")
+        time.sleep(3)
+        opt = chan.recv(9999)
+ssh.close()
