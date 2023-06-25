9ed0e5ee2d53e9e13a7b09676ecef384e62a8a6a
vidyadhar <vidyadhar.bth@gmail.com>
2023-06-25 00:08:31 -0400

    Update nova_neutron_restart.sh

--- nova_neutron_restart.sh
+++ nova_neutron_restart.sh
@@ -1,5 +1,15 @@
-for node_name in $(fuel node list | grep compute | awk '{print $5}' ); 
-do
-    echo Node: $node_name
-    ssh ${node_name} "service nova-compute restart; sleep 5 ; service neutron-openvswitch-agent restart"
+#!/bin/bash
+
+restart_services() {
+    local node_name=$1
+    echo "Node: $node_name"
+    ssh "$node_name" "service nova-compute restart; sleep 5; service neutron-openvswitch-agent restart"
+}
+
+# Get the list of node names
+node_names=$(fuel node list | grep compute | awk '{print $5}')
+
+# Iterate over each node name and restart services
+for node_name in $node_names; do
+    restart_services "$node_name"
 done
