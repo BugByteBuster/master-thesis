43db73015a8ba8b447862f93915e0026a0bbc54f
vidyadhar <vidyadhar.bth@gmail.com>
2023-06-25 00:19:12 -0400

    Update send_files.sh

--- test1/send_files.sh
+++ test1/send_files.sh
@@ -1,30 +1,30 @@
+send_files() {
+  local source_dir=$1
+  local destination_dir=$2
+  local exception_file=$3
+
+  cd "$source_dir"
+  local files=$(ls -1tr)
+  local exception=$(echo "$files" | tail -1)
+
+  for file in $files; do
+    if [[ $file == $exception ]]; then
+      continue
+    fi
+
+    scp -r "$file" "root@SC-1:$destination_dir"
+    rm -rf "$file"
+  done
+
+  cd ..
+}
+
 cd packets
-exception=$(ls -1tr | tail -1)
-for fn in *; do
-  if [[ $fn == $exception ]]; then
-    continue
-  fi
-  scp -r "$fn" root@SC-1:/root/packets/ 
-  rm -rf "$fn"
-done
-cd ..
+send_files . /root/packets
 
-#send files from PL-3 to SC-1: remember to write tcpdump output to a folder ex:packets
-#make sure that SC-1 has an empty folder named packets
-
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 cd /cluster/packets
-exception=$(ls -1tr | tail -1)
-for fn in *; do
-  if [[ $fn == $exception ]]; then
-    continue
-  fi
-  scp -r "$fn" root@SC-1:/dev/packets/ 
-  rm -rf "$fn"
-done
-cd ..
+send_files . /dev/packets
+
 echo "completed"
 
-
 nohup timeout 43200 tcpdump -i eth0 greater 1500 -W 5000 -C 600 -w /cluster/packets/packetinfo >/dev/null 2>&1
-
