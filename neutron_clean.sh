4fc248aa60f0f283b2ebed3e6a5cc8d8a5e9530b
vidyadhar <vidyadhar.bth@gmail.com>
2023-06-25 00:07:28 -0400

    Update neutron_clean.sh
    functions and better formatting

--- neutron_clean.sh
+++ neutron_clean.sh
@@ -1,17 +1,30 @@
-for PORT in $(neutron port-list | grep -v "atlas" | awk '{print $2}')
-do
-    echo Deleting PORT: $PORT
-    neutron port-delete $PORT
-done
+#!/bin/bash
 
-for subnet in $(neutron subnet-list | grep -v "provider" | awk '{print $2}')
-do
-    echo Deleting subnet $subnet
-    neutron subnet-delete $subnet
-done
+delete_ports() {
+    for PORT in $(neutron port-list | grep -v "atlas" | awk '{print $2}')
+    do
+        echo "Deleting PORT: $PORT"
+        neutron port-delete $PORT
+    done
+}
 
-for net in $(neutron net-list | grep -v "provider" | awk '{print $2}')
-do
-    echo Deleting net $net
-    neutron net-delete $net
-done 
+delete_subnets() {
+    for subnet in $(neutron subnet-list | grep -v "provider" | awk '{print $2}')
+    do
+        echo "Deleting subnet $subnet"
+        neutron subnet-delete $subnet
+    done
+}
+
+delete_networks() {
+    for net in $(neutron net-list | grep -v "provider" | awk '{print $2}')
+    do
+        echo "Deleting net $net"
+        neutron net-delete $net
+    done
+}
+
+# Call the functions
+delete_ports
+delete_subnets
+delete_networks
