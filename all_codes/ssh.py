db8f88f8908de5078ca28b6320630b597d585975
vidyadhar <vidyadhar.bth@gmail.com>
2023-06-25 00:31:34 -0400

    Update ssh.py

--- all_codes/ssh.py
+++ all_codes/ssh.py
@@ -1,23 +1,41 @@
-# -*- coding: utf-8 -*-
 import paramiko
 import time
 
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(hostname='10.80.100.102', username='root', password='rootroot')
+def connect_ssh(hostname, username, password):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname=hostname, username=username, password=password)
+    return ssh
 
-channel = ssh.invoke_shell()
-for i in range(0, 5000):
-	print "fetching files for the {}st time".format(i)
-	channel.send("/cluster/./send_files.sh\n")
+def fetch_files(ssh):
+    channel = ssh.invoke_shell()
+    for i in range(0, 5000):
+        print("fetching files for the {}st time".format(i))
+        channel.send("/cluster/./send_files.sh\n")
         while True:
-        	resp = chan.recv(9999)
-		print resp
-		if "password:" in resp:
-                	chan.send("9505100403@usha\n")
-		if "SC-1:" in resp:
-			break
-        chan.send("exit\n")
+            resp = channel.recv(9999)
+            print(resp)
+            if "password:" in resp:
+                channel.send("xxxxxxxx\n")
+            if "SC-1:" in resp:
+                break
+        channel.send("exit\n")
         time.sleep(3)
-        opt = chan.recv(9999)
-ssh.close()
+        opt = channel.recv(9999)
+
+def close_ssh(ssh):
+    ssh.close()
+
+# SSH connection details
+hostname = '10.80.100.102'
+username = 'xxxx'
+password = 'xxxx'
+
+# Connect to SSH
+ssh = connect_ssh(hostname, username, password)
+
+# Fetch files
+fetch_files(ssh)
+
+# Close SSH connection
+close_ssh(ssh)
