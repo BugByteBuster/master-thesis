0d199029ff80609a82ac72860293abd666a06cc4
vidyadhar <vidyadhar.bth@gmail.com>
2023-06-25 00:10:39 -0400

    Update ssh_download.py

--- ssh_download.py
+++ ssh_download.py
@@ -1,15 +1,45 @@
-# -*- coding: utf-8 -*-
 import paramiko
 
-ssh_client=paramiko.SSHClient()
-ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-connection=ssh_client.connect(hostname="192.168.xx.xx",username="ezpedvi",password="***********")
-print connection
+def connect_ssh(hostname, username, password):
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_client.connect(hostname=hostname, username=username, password=password)
+    return ssh_client
 
-stdin,stdout,stderr=ssh_client.exec_command("ls")
-print(stdout.readlines())
+def execute_command(ssh_client, command):
+    stdin, stdout, stderr = ssh_client.exec_command(command)
+    return stdout.readlines()
 
-ftp_client=ssh_client.open_sftp()
-ftp_client.get('scr.sh','/home/vidyadhar/scr.sh')
-ftp_client.close()
-stdin,stdout,stderr=ssh_client.exec_command("rm -rf scr.sh")
+def download_file(ssh_client, remote_path, local_path):
+    ftp_client = ssh_client.open_sftp()
+    ftp_client.get(remote_path, local_path)
+    ftp_client.close()
+
+def remove_file(ssh_client, remote_path):
+    ssh_client.exec_command("rm -rf " + remote_path)
+
+def main():
+    hostname = "192.168.xx.xx"
+    username = "XXXXX"
+    password = "XXXXX"
+
+    ssh_client = connect_ssh(hostname, username, password)
+    print("SSH connection established.")
+
+    command = "ls"
+    output = execute_command(ssh_client, command)
+    print(output)
+
+    remote_path = "scr.sh"
+    local_path = "/home/vidyadhar/scr.sh"
+    download_file(ssh_client, remote_path, local_path)
+
+    remove_file(ssh_client, remote_path)
+
+    ssh_client.close()
+    print("SSH connection closed.")
+
+
+if __name__ == "__main__":
+    main()
+
