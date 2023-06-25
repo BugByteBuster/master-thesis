041834ca31aafb71b5bb9f03bff714ab8a4c3697
vidyadhar <vidyadhar.bth@gmail.com>
2023-06-25 00:28:11 -0400

    Update get_files.sh

--- all_codes/get_files.sh
+++ all_codes/get_files.sh
@@ -1,62 +1,57 @@
-# -*- coding: utf-8 -*-
 import paramiko
 import time
-import re
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(hostname='134.138.212.12', username='ezpedvi', password='9505100403@usha')
-chan = ssh.invoke_shell()
 
+def ssh_connect(hostname, username, password):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname=hostname, username=username, password=password)
+    return ssh
 
-for i in range(0, 5000):
-	print "fetching files for the {}st time".format(i)
-    	#ssh from ez to sc1
-    	command = chan.send("ssh root@10.80.68.37\n")
-    	time.sleep(5)
-    	opt = chan.recv(9999)
-    	print "opt{}".format(opt)
-    	chan.send("rootroot\n")
-    	time.sleep(3)
-    	opt2 = chan.recv(9999)
-    	print opt2
-    	#ssh from  sc1 to pl3
-    	chan.send("ssh PL-3\n")
-    	time.sleep(3)
-    	opt3 = chan.recv(9999)
-    	print opt3
+def ssh_command(chan, command, sleep_time=3):
+    chan.send(command + '\n')
+    time.sleep(sleep_time)
+    return chan.recv(9999)
 
-    	#file transfer Pl-3 to sc-1
-    	tstart = time.time()
-    	chan.send("/cluster/./send_files.sh\n")
-	while True:
-		print "while loop started....."
-		resp = chan.recv(9999)
-		print "resp {} ".format(resp)
-		if "PL-3:~"  in resp:
-			break
-		else:
-			print "gone"
-	time.sleep(3)
-    	chan.send("exit\n")
-    	time.sleep(3)
-    	opt4=chan.recv(9999)
-    	print opt4
-    	tend=time.time()
-    	#file transfer from sc1- ez
-    	chan.send("scp -r /dev/packets/* ezpedvi@134.138.212.12:/home/ezpedvi/packets/\n")
-    	time.sleep(2)
-    	chan.send("9505100403@usha\n")
-    	print tend-tstart
-    	time.sleep(tend-tstart)
-    	chan.send("wait\n")
-    	time.sleep(2)
-    	opt5=chan.recv(9999)
-    	print opt5
-    	chan.send("rm -rf /dev/packets/*\n")
-    	time.sleep(10)
-    	chan.send("exit\n")
-    	time.sleep(3)
-    	opt6=chan.recv(9999)
-    	print opt6
+def transfer_files(chan, source, destination):
+    chan.send("scp -r {} {}\n".format(source, destination))
+    time.sleep(2)
 
-ssh.close()
+def main():
+    hostname = 'x.x.x.x'
+    username = 'xxxxxxx'
+    password = 'xxxxxxx'
+
+    ssh = ssh_connect(hostname, username, password)
+    chan = ssh.invoke_shell()
+
+    for i in range(0, 5000):
+        print("fetching files for the {}st time".format(i))
+        
+        # SSH from ez to sc1
+        ssh_command(chan, "ssh root@10.80.68.37")
+        opt = ssh_command(chan, "rootroot")
+        
+        # SSH from sc1 to pl3
+        opt3 = ssh_command(chan, "ssh PL-3")
+
+        # File transfer Pl-3 to sc-1
+        tstart = time.time()
+        while True:
+            resp = ssh_command(chan, "/cluster/./send_files.sh")
+            print("resp: {}".format(resp))
+            if "PL-3:~" in resp:
+                break
+        opt4 = ssh_command(chan, "exit")
+        tend = time.time()
+
+        # File transfer from sc1- ez
+        transfer_files(chan, "/dev/packets/*", "xxxxxx@x.x.x.x:/home/ezpedvi/packets/")
+        time.sleep(tend - tstart)
+        ssh_command(chan, "wait")
+        opt5 = ssh_command(chan, "rm -rf /dev/packets/*")
+        opt6 = ssh_command(chan, "exit")
+
+    ssh.close()
+
+if __name__ == "__main__":
+    main()
