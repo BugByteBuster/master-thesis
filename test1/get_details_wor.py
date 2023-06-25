a32ee48d139e294c987eeb6815f5df5997368013
vidyadhar <vidyadhar.bth@gmail.com>
2023-06-25 00:22:51 -0400

    Update get_details_wor.py

--- test1/get_details_wor.py
+++ test1/get_details_wor.py
@@ -1,32 +1,50 @@
-# -*- coding: utf-8 -*-
 import paramiko
 import time
-import re
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(hostname='x.x.x.x', username='ezpedvi', password='xxxxxxxx')
 
-chan = ssh.invoke_shell()
+def connect_ssh(hostname, username, password):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname=hostname, username=username, password=password)
+    return ssh
 
-for i in range(0, 5000):
-	print "fetching files for the {}st time".format(i)
-	command = chan.send("ssh root@10.80.68.37\n")
-	time.sleep(5)
-    	opt = chan.recv(9999)
-    	print "opt{}".format(opt)
-    	chan.send("rootroot\n")
-    	time.sleep(3)
-    	opt2 = chan.recv(9999)
-    	print opt2
-	chan.send("/cluster/./send_files.sh\n")
+def execute_command(ssh, command, delay):
+    chan = ssh.invoke_shell()
+    chan.send(command)
+    time.sleep(delay)
+    output = chan.recv(9999)
+    return output
+
+def fetch_files(hostname, username, password, target_ip, target_password):
+    ssh = connect_ssh(hostname, username, password)
+    
+    for i in range(5000):
+        print("fetching files for the {}st time".format(i))
+        
+        output = execute_command(ssh, "ssh root@{}\n".format(target_ip), 5)
+        print("opt: {}".format(output))
+        
+        output = execute_command(ssh, "rootroot\n", 3)
+        print(output)
+        
+        output = execute_command(ssh, "/cluster/./send_files.sh\n", 0)
         while True:
-        	resp = chan.recv(9999)
-		print resp
-		if "password:" in resp:
-                	chan.send("xxxxxxxxxx\n")
-		if "SC-1:" in resp:
-			break
-        chan.send("exit\n")
-        time.sleep(3)
-        opt = chan.recv(9999)
-ssh.close()
+            resp = chan.recv(9999)
+            print(resp)
+            if "password:" in resp:
+                chan.send("{}\n".format(target_password))
+            if "SC-1:" in resp:
+                break
+        
+        output = execute_command(ssh, "exit\n", 3)
+        print(output)
+    
+    ssh.close()
+
+if __name__ == "__main__":
+    hostname = 'x.x.x.x'
+    username = 'ezpedvi'
+    password = 'xxxxxxxx'
+    target_ip = 'x.x.x.x'
+    target_password = 'xxxxxxxxxx'
+    
+    fetch_files(hostname, username, password, target_ip, target_password)
