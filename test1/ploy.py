7c0d9adf093dd1a5e05d545b801798cf6a698848
vidyadhar <vidyadhar.bth@gmail.com>
2023-06-25 00:25:06 -0400

    Update ploy.py

--- test1/ploy.py
+++ test1/ploy.py
@@ -1,54 +1,54 @@
-import numpy
+import numpy as np
 import os
 import dpkt
 import matplotlib.pyplot as plt
 import datetime
 import collections
 
-timestamps=[]
-mtu=[]
+def read_pcap_files(directory):
+    timestamps = []
+    mtu = []
 
-for filename in os.listdir('/home/vidyadhar/packets'):
-   if filename.startswith('packet'):
-    print filename
-        with open(os.path.join('/home/vidyadhar/packets', filename)) as f:
-            pcap = dpkt.pcap.Reader(f)
-        for timestamp, buf in pcap:
-            ip = dpkt.ethernet.Ethernet(buf).data
-        #tcp = ip.data
-           time=datetime.datetime.fromtimestamp(int(timestamp)).strftime('%H')
-             timestamps.append(time)
-        mtu.append(len(dpkt.ethernet.Ethernet(buf)))
-print "length of timestamps {}".format(len(timestamps))
-print "lenght of mtus{}".format(len(mtu)) #count of total packets received
+    for filename in os.listdir(directory):
+        if filename.startswith('packet'):
+            with open(os.path.join(directory, filename)) as f:
+                pcap = dpkt.pcap.Reader(f)
+            for timestamp, buf in pcap:
+                ip = dpkt.ethernet.Ethernet(buf).data
+                timestamps.append(datetime.datetime.fromtimestamp(int(timestamp)).strftime('%H'))
+                mtu.append(len(dpkt.ethernet.Ethernet(buf)))
+    return timestamps, mtu
 
-#print mtu (print length of all the received packets)
-#these many packets received in this hour
-print collections.Counter(timestamps)
-#print counter
+def plot_timestamp_counts(timestamps):
+    counter = collections.Counter(timestamps)
+    labels, values = zip(*counter.items())
+    indexes = np.arange(len(labels))
 
-#frequency of packets with length greater than 1500 and range between 1500 - 6500
-counts, bins = numpy.histogram(mtu, bins=10, range=(1500, 6500))
-print "range {}".format(bins)
-print "frequency {}".format(counts)
+    plt.bar(indexes, values)
+    plt.xticks(indexes, labels)
+    plt.xlabel('Time')
+    plt.ylabel('Count')
+    plt.show()
 
-labels, values = zip(*collections.Counter(timestamps).items())
-indexes = numpy.arange(len(labels))
-width=1
+def plot_packet_frequency(mtu):
+    counts, bins = np.histogram(mtu, bins=10, range=(1500, 6500))
 
-#plt.bar(indexes, values, width)
-plt.bar(indexes, values)
-#plt.xticks(indexes+width*0.5, labels)
-plt.xticks(indexes, labels)
-plt.show()
+    plt.bar(bins[:-1], counts)
+    plt.xlabel('Packet Length')
+    plt.ylabel('Frequency')
+    plt.show()
 
+def main():
+    timestamps, mtu = read_pcap_files('/home/vidyadhar/packets')
+    
+    print("Length of timestamps: {}".format(len(timestamps)))
+    print("Length of mtus: {}".format(len(mtu)))
+    
+    counter = collections.Counter(timestamps)
+    print("Counter: {}".format(counter))
+    
+    plot_timestamp_counts(timestamps)
+    plot_packet_frequency(mtu)
 
-
-'''
-#print timestamps
-plt.bar(timestamps,len(mtu))
-plt.xlabel('time')
-plt.ylabel('count')
-plt.show()
-plt.savefig('plot.png')
-'''
+if __name__ == "__main__":
+    main()
